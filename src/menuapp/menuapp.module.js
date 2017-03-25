a90a049d648ddbef5b1456a9afc8974eea973c6b
Lawrence McDaniel <lpm0073@gmail.com>
2017-03-25 07:34:00 -0600

    working solution

--- src/menuapp/menuapp.module.js
+++
@@ -1,9 +0,0 @@
-// declare an Angular module to match your ng-app declaration.
-
-(function () {
-'use strict';
-
-//angular.module('ShoppingList', ['ui.router', 'Spinner']);
-angular.module('MenuApp', ['ui.router', 'Spinner', 'Data']);
-
-})();
